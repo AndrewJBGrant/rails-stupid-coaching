272eacb0e2f7600bef2a85bd960f9afc67244981
Andrew Grant <andrewgrant2009@hotmail.com>
2022-10-06 17:22:31 +0200

    complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,19 @@
-good_question = 'I am going to work'
-good_answer = 'Great!'
-silly_answer = 'Silly question, get dressed and go to work!'
-mean_answer = 'I dont care, get dressed and go to work!'
-
 class QuestionsController < ApplicationController
-  def home
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
   end
 
   def ask
   end
 
-  def answer
-    @question = params[:question]
-    @answer = stupid_coach(@question)
-end
-
-private
-
-  def stupid_coach(str)
-    question?(str) ? silly_answer : (str === good_question ? good_answer : mean_answer)
-  end
-
-  def question?(str)
-    (str === nil) ? false : str.end_with?("?")
+  def coach_answer(question)
+    if question == 'I am going to work'
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      'I dont care, get dressed and go to work!'
     end
+  end
 end
